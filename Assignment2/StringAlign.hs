5284a2f3553e0ccf2cf517ec2adb6e5a25ddc9b2
JensMellberg <dat14jme@student.lu.se>
2018-05-10 22:19:52 +0200

    Fixat lite

--- Assignment2/StringAlign.hs
+++ Assignment2/StringAlign.hs
@@ -10,27 +10,12 @@ string2 = "vintner"
 type AlignmentType = (String,String)
 
 
-similarityScoreSlow :: String -> String -> Int
-similarityScoreSlow [] _ = scoreSpace
-similarityScoreSlow _ [] = scoreSpace
-similarityScoreSlow (x:xs) (y:ys) = maximum [
-                              similarityScoreSlow xs ys + score2 (x, y),
-                              similarityScoreSlow (x:xs) ys + score2 ('-', y),
-                              similarityScoreSlow xs (y:ys) + score2 (x, '-')]
-
 similarityScore :: String -> String -> Int
 similarityScore [] [] = 0
 similarityScore (s1:string1) [] = score s1 '-' * length (s1:string1)
 similarityScore [] (s2:string2) = score '-' s2 * length (s2:string2)
 similarityScore (s1:string1) (s2:string2) = maximum [similarityScore string1 string2 + score s1 s2, similarityScore string1 (s2:string2) + score s1 '-', similarityScore (s1:string1) string2 + score '-' s2]
 
-
-score2 :: (Char, Char) -> Int
-score2 (x, '-') = scoreSpace
-score2 ('-', y) = scoreSpace
-score2 (x, y)
- | x == y    = scoreMatch
- | otherwise = scoreMismatch
 
 score x y
  | x == '-' || y == '-' = scoreSpace
@@ -44,24 +29,22 @@ stringScore :: (String, String) -> Int
 stringScore ([], []) = 0
 stringScore (x:xs, y:ys) = score x y + stringScore (xs, ys)
 
---optAlignmentsSlow :: String -> String -> [AlignmentType]
---optAlignmentsSlow xs ys = maximaBy stringScore $ findAlignments xs ys
 
---findAlignments [] [] = [("","")]
---findAlignments [] ys = attachHeads '-' (head ys) [("","")]
---findAlignments xs [] = attachHeads (head xs) '-' [("","")]
---findAlignments (x:xs) (y:ys) = concat [
---      attachHeads x y $ findAlignments xs ys,
---      attachHeads x '-' $ findAlignments xs (y:ys),
---      attachHeads '-' y $ findAlignments (x:xs) ys]
+outputOptAlignments string1 string2 = do
+  let opts = optAlignmentsSlow string1 string2
+  putStrLn $ "There are " ++ (show . length $ opts) ++ " optimal alignments."
+  putStrLn $ concat (map outputSingle opts)
+
+outputSingle (string1, string2) = "\n"++string1 ++ "\n"++string2 ++ "\n"
+
 
 optAlignmentsSlow :: String -> String -> [AlignmentType]
 optAlignmentsSlow xs ys = maximaBy stringScore $ findAlignments xs ys
-  where
-    findAlignments [] [] = [("","")]
-    findAlignments [] ys = attachHeads '-' (head ys) [("","")]
-    findAlignments xs [] = attachHeads (head xs) '-' [("","")]
-    findAlignments (x:xs) (y:ys) = concat [
+
+findAlignments [] [] = [("","")]
+findAlignments [] ys = [(replicate (length ys) '-', ys)]
+findAlignments xs [] = [(xs, replicate (length xs) '-')]
+findAlignments (x:xs) (y:ys) = concat [
       attachHeads x y $ findAlignments xs ys,
       attachHeads x '-' $ findAlignments xs (y:ys),
       attachHeads '-' y $ findAlignments (x:xs) ys]
@@ -72,5 +55,5 @@ maximaBy func xs = foldr (maxima func) [] xs
 maxima func x [] = [x]
 maxima func x (a:acc)
  | func x >  func a = [x]
- | func x == func a = x:(a:acc)
+ | func x == func a = (x:a:acc)
  | otherwise = (a:acc)
