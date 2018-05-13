4f5cd14b10dce87a3ebc168e8845f0d62f07cefe
JensMellberg <dat14jme@student.lu.se>
2018-05-13 13:37:29 +0200

    Putsade koden lite

--- Assignment2/StringAlign.hs
+++ Assignment2/StringAlign.hs
@@ -8,6 +8,7 @@ string1 = "writers"
 string2 = "vintner"
 
 type AlignmentType = (String,String)
+type AlignmentPair = (Int, [AlignmentType])
 
 
 similarityScoreOld :: String -> String -> Int
@@ -32,64 +33,34 @@ similarityScore string1 string2 = simLen (length string1) (length string2)
           l1 = length string1
           l2 = length string2
 
---optAlignments :: String -> String -> [AlignmentType]
---optAlignments string1 string2 = snd $ simLen (length string1) (length string2)
---  where
---    simLen i j = simTable!!i!!j
---    simTable = [[ alignmentEntry i j | j<-[0..]] | i<-[0..] ]
---
---    alignmentEntry 0 0 = (0,[("","")])
---    alignmentEntry 0 ys = (ys * scoreSpace, [(replicate ys '-', takeLast ((length string2) - ys) string2)])
---    alignmentEntry xs 0 = (xs * scoreSpace, [(takeLast ((length string1) - xs) string1, replicate xs '-')])
---    alignmentEntry xs ys = 1 !! res
---       where
---         x = string1 !! ((length string1) - xs)
---         y = string2 !! ((length string2) - ys)
---         entry1 = attachOnPair (score x y) x y simLen (xs-1) (ys-1)
---         entry2 = attachOnPair scoreSpace x '-' simLen (xs-1) ys
---         entry3 = attachOnPair scoreSpace '-' y simLen xs (ys-1)
---         res = maximaBy fst [entry1, entry2, entry3]
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments string1 string2 = snd $ optAl (length string1) (length string2)
+  where
+    optAl i j = optTable!!i!!j
+    optTable = [[ alignmentEntry i j | j<-[0..]] | i<-[0..] ]
 
--- [(1,alignmenttype), ]
+    alignmentEntry :: Int -> Int -> AlignmentPair
+    alignmentEntry 0 0 = (0,[("","")])
+    alignmentEntry 0 ys = (ys * scoreSpace, [(replicate ys '-', takeLast ys string2)])
+    alignmentEntry xs 0 = (xs * scoreSpace, [(takeLast xs string1, replicate xs '-')])
+    alignmentEntry xs ys = foldl fuse (0, []) $ maximaBy fst [entry1, entry2, entry3]
+       where
+         x = string1 !! (length string1 - xs)
+         y = string2 !! (length string2 - ys)
+         entry1 = attachOnPair (score x y) x y $ optAl (xs-1) (ys-1)
+         entry2 = attachOnPair scoreSpace x '-' $ optAl (xs-1) ys
+         entry3 = attachOnPair scoreSpace '-' y $ optAl xs (ys-1)
+         res = maximaBy fst [entry1, entry2, entry3]
 
-attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
-attachTails h1 h2 li = [((xs ++ [h1]), (ys ++ [h2])) | (xs,ys) <- li]
+attachOnPair :: Int -> Char -> Char -> AlignmentPair -> AlignmentPair
+attachOnPair s x y (score, aTypes) = (s + score, attachHeads x y aTypes)
+fuse :: AlignmentPair -> AlignmentPair -> AlignmentPair
+fuse (s1, type1) (s2, type2) = (s2, type1 ++ type2)
 
-fastOptAlignments :: String -> String -> [AlignmentType]
-fastOptAlignments xs ys = snd $ optAlign (length xs) (length ys)
-    where
-    optAlign i j = optTable!!i!!j
-    optTable :: [[(Int, [AlignmentType])]]
-    optTable = [[ optEntry i j | j<-[0..]] | i<-[0..]]
-
-    optEntry :: Int -> Int -> (Int, [AlignmentType])
-    optEntry 0 0 = (0, [([],[])])
-    optEntry i 0 = (scoreSpace + fst (optEntry (i-1) 0), attachTails (xs!!(i-1)) '-' $ snd (optEntry (i-1) 0))
-    optEntry 0 j = (scoreSpace + fst (optEntry 0 (j-1)), attachTails '-' (ys!!(j-1)) $ snd (optEntry 0 (j-1)))
-    optEntry i j = (fst (best!!0), concat $ map snd best)
-        where
-        best = maximaBy fst [diag, right, down]
-            where
-            diag, right, down :: (Int, [AlignmentType])
-            diag
-                | x == y    = addScoreAndTails scoreMatch x y (optAlign (i-1) (j-1))
-                | otherwise = addScoreAndTails scoreMismatch x y (optAlign (i-1) (j-1))
-            right = addScoreAndTails scoreSpace '-' y (optAlign i (j-1))
-            down = addScoreAndTails scoreSpace x '-' (optAlign (i-1) j)
-            x = xs!!(i-1)
-            y = ys!!(j-1)
-
-            addScoreAndTails :: Int -> Char -> Char -> (Int, [AlignmentType]) -> (Int, [AlignmentType])
-            addScoreAndTails sc a b (score, as) = (score + sc, attachTails a b as)
-
---attachHeads x y $ snd $ alignmentEntry (xs-1) (ys-1),
---attachHeads x '-' $ snd $ alignmentEntry (xs-1) ys,
---attachHeads '-' y $ snd $ alignmentEntry xs (ys-1)]
-
-attachOnPair s x y (score, aTypes) = (s + score, attachHeads x y $ aTypes)
-
+takeLast :: Int -> String -> String
 takeLast n = reverse . take n . reverse
 
+score :: Char -> Char -> Int
 score x y
  | x == '-' || y == '-' = scoreSpace
  | x == y = scoreMatch
@@ -104,9 +75,10 @@ stringScore (x:xs, y:ys) = score x y + stringScore (xs, ys)
 
 
 outputOptAlignments string1 string2 = do
-  let opts = optAlignmentsSlow string1 string2
+  let opts = optAlignments string1 string2
   putStrLn $ "There are " ++ (show . length $ opts) ++ " optimal alignments."
-  putStrLn $ concat (map outputSingle opts)
+  putStrLn $ concatMap outputSingle opts
+  putStrLn $ "There were " ++ (show . length $ opts) ++ " optimal alignments."
 
 outputSingle (string1, string2) = "\n"++string1 ++ "\n"++string2 ++ "\n"
 
@@ -123,10 +95,10 @@ findAlignments (x:xs) (y:ys) = concat [
       attachHeads '-' y $ findAlignments (x:xs) ys]
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
-maximaBy func xs = foldr (maxima func) [] xs
+maximaBy func = foldr (maxima func) []
 
 maxima func x [] = [x]
 maxima func x (a:acc)
  | func x >  func a = [x]
- | func x == func a = (x:a:acc)
- | otherwise = (a:acc)
+ | func x == func a = x:a:acc
+ | otherwise = a:acc
